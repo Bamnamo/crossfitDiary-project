b2dd402923ad8d4d2af032bc26802ce3ec80b324
박성범 <shivash@naver.com>
2020-11-20 14:24:42 +0900

    1. 프로젝트 설정

---
+++ src/main/java/crossfit/crossfitdiary/CrossfitDiaryApplication.java
@@ -0,0 +1,13 @@
+package crossfit.crossfitdiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrossfitDiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrossfitDiaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/crossfit/crossfitdiary/CrossfitDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package crossfit.crossfitdiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrossfitDiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
